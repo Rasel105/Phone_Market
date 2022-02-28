4bac3b43d23b50c6b3c56008ef7fdec6ee446744
Rasel <azad.is.rasel@gmail.com>
2022-02-28 21:30:13 +0600

    fetch data and fetch it

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-console.log("hello")
+const getPhone = () => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=samsung`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhone(data.data))
+}
+getPhone()
+
+const displayPhone = (phones) => {
+    // console.log(phones)
+    phones.forEach(phone => {
+        console.log(phone)
+    })
+}
