088f6b8ff07d2cb19c65f1f8899fe324eb1abff9
Rasel <azad.is.rasel@gmail.com>
2022-03-01 20:33:01 +0600

    Not found error handle

--- js/index.js
+++ js/index.js
@@ -1,34 +1,48 @@
+
+const spinner = document.getElementById('spinner-secion');
+const error = document.getElementById('error')
 const getPhone = () => {
     const inputValue = document.getElementById('input-value')
     let inputValueData = inputValue.value;
-    const error = document.getElementById('error')
+    // const error = document.getElementById('error')
     // alert('Please enter a number')
     if (inputValueData == '') {
         error.innerHTML = `
         <div class="alert alert-danger alert-dismissible ">
         <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-        <strong>Oohs!</strong> Type your desire phone.
+        <strong>Ohps!</strong> Type your desire phone.
         </div>
         `
     }
-
     else {
-        const url = `https://openapi.programming-hero.com/api/phones?search=${inputValueData}`
+        const url = `https://openapi.programming-hero.com/api/phones?search=${inputValueData}`;
         fetch(url)
             .then(res => res.json())
             .then(data => displayPhone(data.data))
+        spinner.innerHTML = `
+            <div class="text-center">
+         <div class="spinner-border" role="status">
+         <span class="visually-hidden">Loading...</span>
+         </div>
+         </div>
+            `
     }
-
-
-
 }
 
 const displayPhone = (phones) => {
     // console.log(phones)
+    if (phones.length == 0) {
+        error.innerHTML = `
+         <div class="alert alert-danger alert-dismissible ">
+        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+        <strong>Ohps Sorry! </strong> Not Found!
+        </div>
+        `
+    }
     const displayPhoneData = document.getElementById('main-details');
     displayPhoneData.innerHTML = ''
     phones.forEach(phone => {
-        console.log(phone)
+        // console.log(phone)
         // const phone20 = phone.slice(0, 20);
         // console.log(phone20)
         const div = document.createElement('div');
@@ -46,6 +60,7 @@ const displayPhone = (phones) => {
           </div> `
         displayPhoneData.appendChild(div)
     })
+    spinner.innerHTML = '';
 }
 
 const cardDetails = (detailsValue) => {
@@ -56,13 +71,13 @@ const cardDetails = (detailsValue) => {
 }
 
 const showDetailOnTop = (phone) => {
-    console.log(phone)
+    // console.log(phone)
     const detailTag = document.getElementById('details-section');
     detailTag.innerHTML = '';
     const div = document.createElement('div');
     div.innerHTML = `
-     <div class="card w-25 mx-auto">
-           <div class="p-3"><img src="${phone.image}" class="card-img-top"> </div>
+     <div class="card w-50 mx-auto">
+           <div class="p-3 w-50 mx-auto"><img src="${phone.image}" class="card-img-top"></div>
            <h4 class="text-center text-info">Details</h4>
             <div class="card-body">
             <h6 class="card-title  my-0"><span class="fw-bold">Name: </span><span class="text-success">${phone.name}</span></h6>
@@ -71,10 +86,9 @@ const showDetailOnTop = (phone) => {
             <p class="card-text  my-0"><span class="fw-bold">Storage: </span><span class="text-success">${phone.mainFeatures.storage}</span></p>
             <p class="card-text  my-0"><span class="fw-bold">Display: </span><span class="text-success">${phone.mainFeatures.displaySize}</span></p>
             <p class="card-text  my-0"><span class="fw-bold">Sensors: </span><span class="text-success">${phone.mainFeatures.sensors}</span></p>
-            <p class="card-text  my-0"><span class="fw-bold">Bluetooth: </span><span class="text-success">${phone.others.GPS}</span></p>
-            <p class="card-text  my-0"><span class="fw-bold">GPS: </span><span class="text-success">${phone.others.WLAN}</span></p>
+            <p class="card-text  my-0"><span class="fw-bold">Bluetooth: </span><span class="text-success">${phone?.others?.GPS}</span></p>
+            <p class="card-text  my-0"><span class="fw-bold">GPS: </span><span class="text-success">${phone?.others?.WLAN}</span></p>
       </div> 
         `
     detailTag.appendChild(div)
-
 }
